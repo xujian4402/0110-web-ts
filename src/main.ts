967d3d9af3d65f061b57032352b5a930ce4a3a28
xujian <xujian4402@hotmail.com>
2019-10-15 19:21:29 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import Vue from 'vue';
+
+import 'normalize.css';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+import '@/permission';
 
 Vue.config.productionTip = false;
 
